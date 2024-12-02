9fcd1b7015e5779524c51d4b96ecb85564f7ec40
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2024-12-02 22:27:10 +0000

    Create AnimatedCircularGradient.h

---
+++ AnimatedCircularGradient.h
@@ -0,0 +1,9 @@
+uniform vec2 resolution;
+uniform float time;
+
+void main() {
+    vec2 uv = gl_FragCoord.xy / resolution.xy;
+    vec2 center = uv - 0.5;
+    float radius = length(center) * 2.0;
+    gl_FragColor = vec4(radius, 0.5 - radius, abs(sin(time)), 1.0);
+}
